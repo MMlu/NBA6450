f4415d60b1605fab07d42494d62e49b5b8332a61
acw82 <acw82@cornell.edu>
2016-09-10 16:53:26 -0400

    Files from Adrian

--- PS1_2.py
+++ PS1_2.py
@@ -7,7 +7,7 @@ import statsmodels.tsa.api as stats
 
 #Part A
 
-df = pd.read_csv('vwret.csv')
+df = pd.read_csv('Data/vwret.csv')
 
 df.ix[0, 'price'] = 1 + df.ix[0, 'vwretx']
 df.ix[0, 'div'] = df.ix[0,'vwretd'] - df.ix[0,'vwretx']
@@ -25,28 +25,28 @@ for i in range(0,len(df)):
     df.ix[i, 'pd_ratio'] = math.log(df.ix[i, 'price']/df.ix[i, 'div_12mo'])
 
 
-df.plot(y='pd_ratio')
+#df.plot(y='pd_ratio')
 #print(df)
 
 #Part B
-df2 = pd.read_csv('CAPE.csv')
+df2 = pd.read_csv('Data/CAPE.csv')
 df2['log_CAPE'] = np.log(df2['CAPE'])
-#print(df2)
+print(len(df2))
 
 df['log_CAPE']= df2['log_CAPE']
 
-corr = np.corrcoef(df[13:]['pd_ratio'],df[13:]['log_CAPE'])
-print df
-df2.plot(y='log_CAPE')
+corr = np.corrcoef(df[12:]['pd_ratio'],df[12:]['log_CAPE'])
+#print(df)
+#df2.plot(y='log_CAPE')
 
-#print("correlation is: " + str(corr[0,1]))
+print("correlation is: " + str(corr[0,1]))
 
 #Part C
-auto_regress_pd = stats.AR(df[13:]['pd_ratio'].values)
+auto_regress_pd = stats.AR(df[12:]['pd_ratio'].values)
 results = auto_regress_pd.fit(1)
 print(results.params)
 
-auto_regress_cape = stats.AR(df[13:]['log_CAPE'].values)
+auto_regress_cape = stats.AR(df[12:]['log_CAPE'].values)
 results2 = auto_regress_cape.fit(1)
 print(results2.params)
 
@@ -57,11 +57,11 @@ half_life_cape = np.log(0.5)/np.log(results2.params[1])
 print("CAPE half life is: ", half_life_cape)
 
 #Part D
-pd_ols = sm.ols(formula='returns ~ pd_ratio',data=df).fit()
-cape_ols = sm.ols(formula='returns ~ log_CAPE',data=df).fit()
+pd_dickey = stats.adfuller(df[12:]['pd_ratio'].values,1)[1]
+print(pd_dickey)
 
-#print(pd_ols.summary())
-#print(cape_ols.summary())
+cape_dickey = stats.adfuller(df[12:]['log_CAPE'].values,1)[1]
+print(cape_dickey)
 
 
 
