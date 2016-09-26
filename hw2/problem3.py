e12c35bf369969a2104d1c9123000b58d218e7b1
Bryan Lu <kobryanlu@gmail.com>
2016-09-25 20:29:48 -0400

    moving average

--- hw2/problem3.py
+++ hw2/problem3.py
@@ -13,6 +13,10 @@ def get_data(ticker, fromdate, todate):
     timeSeries = DataReader(ticker,  "fred", fromdate, todate)
     return timeSeries
 
+def movingAverage(interval, window_size):
+    window = np.ones(int(window_size))/float(window_size)
+    return np.convolve(interval, window, 'same')
+
 
 ff = pd.read_csv("../Data/FF.csv",index_col =0,parse_dates=True)
 lsc = pd.read_csv('../Data/small_value_ret.csv',index_col =0,parse_dates=True)
@@ -23,9 +27,31 @@ ff = ff.reset_index()
 lsc = lsc.reset_index()
 
 df = pd.concat([ff, lsc], 'i')
-df = df.rename(columns={'Mkt-RF': 'Mkt'})
+df = df.rename(columns={'Mkt-RF': 'MktRF'})
+yearAvg = df.groupby(['year']).sum()
 
-f = ' ret ~ Mkt + SMB + HML'
+df['retRF'] = df['ret'] - df['RF']
+
+print df
+
+# A
+f = ' ret ~ MktRF'
 lm = sm.ols(formula=f, data=df).fit(cov_type='HAC', cov_kwds={'maxlags': 0})
 print lm.params
 
+expectedReturn = lm.params[1] * yearAvg['MktRF'].mean()
+print expectedReturn, '\n'
+
+# B
+f = ' ret ~ MktRF + SMB + HML'
+lm = sm.ols(formula=f, data=df).fit(cov_type='HAC', cov_kwds={'maxlags': 0})
+print lm.params
+
+expectedReturn = lm.params[1] * yearAvg['MktRF'].mean() \
+                    + lm.params[2] * yearAvg['SMB'].mean() \
+                    + lm.params[3] * yearAvg['HML'].mean()
+
+print expectedReturn
+
+# C
+print movingAverage(df['MktRF'], 12)
